9129bef2f1a37d828a3d55a0ed657a9bf2968bdc
Carlos Zuniga <carlos.zuniganajaarro@outlook.com>
2025-07-10 23:13:03 -0700

    Made number guessing game

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+answer = random.randint(1,100)
+tries = 5
+while tries > 0:
+    x = int(input("Guess a number 1- 100:"))
+    if x == answer:
+        print("You're right! You win!")
+        break
+    elif x > answer:
+        print("Too high!")
+        tries -= 1
+    elif x < answer:
+        print("Too low!")
+        tries -= 1
+if tries == 0:
+    print("You lose!")
+    print("The answer was " + str(answer) + ".")
